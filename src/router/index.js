54556543c44fc647779d6a4ace95c53e4d6eb4cc
Kemal Öncel <57701397+kemaloncell@users.noreply.github.com>
2022-04-12 14:15:13 +0300

    create workspace

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter ,createWebHistory} from 'vue-router'
+
+const routes=[
+    {
+    path:'/',
+    name:'HomePage'
+    }
+]
+
+export default createRouter({
+    routes:[],
+    history: createWebHistory
+})
