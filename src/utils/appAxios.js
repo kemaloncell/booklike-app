fb3ca4f7739ee7a579dee5fd53873d16178fb828
Kemal Öncel <57701397+kemaloncell@users.noreply.github.com>
2022-04-13 12:15:47 +0300

    create register ux

--- src/utils/appAxios.js
+++ src/utils/appAxios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export default axios.create({
+export const appAxios =  axios.create({
     baseURL: 'http://localhost:3000',
 })
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="login_register_container">
     <h3 class="text-2xl text-center mb-3">Kayıt Ol</h3>
-    <input type="text" placeholder="Tam Ad" class="input mb-3" />
-    <input type="text" placeholder="Kullanıcı Adı" class="input mb-3" />
-    <input type="password" placeholder="Şifre" class="input mb-3" />
-    <button class="default-button">Kayıt ol</button>
+    <input v-model="userData.username" type="text" placeholder="Tam Ad" class="input mb-3" />
+    <input v-model="userData.fullname" type="text" placeholder="Kullanıcı Adı" class="input mb-3" />
+    <input v-model="userData.password" type="password" placeholder="Şifre" class="input mb-3" />
+    <button @click="onSave" class="default-button">Kayıt ol</button>
     <span class="text-center mt-3 text-sm">
         Zaten Üyeyim,
-        <a href="#" class="text-red-900 hover:text-black">Giriş yap!</a>
+      <router-link :to="{ name: 'Login'}" class="text-red-900 hover:text-black">Giriş yap!</router-link>
       </span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Register',
+  data() {
+    return {
+      userData:{
+        username: null,
+        fullname: null,
+        password: null
+      }
+    };
+  },
+  methods: {
+    onSave() {
+       console.log(this.userData);
+    }
+  }
 }
 </script>
 
