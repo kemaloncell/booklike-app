bec45af8be6ffdc29324214887a9366b32ddbe24
Kemal Öncel <57701397+kemaloncell@users.noreply.github.com>
2022-04-15 13:20:54 +0300

    save the user data on local storge with vue-persestandstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
 import {createStore} from 'vuex';
+import createPersistedState from "vuex-persistedstate";
+import SecureLS from "secure-ls";
+var ls = new SecureLS({ isCompression: false });
 
 export default createStore({
     state: {
@@ -13,11 +16,22 @@ export default createStore({
     getters:{
         _isAuthenticated: state=> state.user !== null,
         _getCurrentUser(state){
-            //  "?" = if userın içinde bir password var ise bunu sil
+            //  "?" = if userın içinde bir password var ise bunu sil password oradan oraya gitmesin güvenlik
             const user = state?.user;
             delete user.password;
             return user;
         },
         _saltKey: state=> state.saltKey
      },
+    // vuex-persistedstate ile store'ın içinde bir değişiklik olursa secure ile şifrelenmiş şekilde localStorage'a kaydedilir
+    plugins: [createPersistedState({
+        storage: {
+            getItem: key => ls.get(key),
+            setItem: (key, value) => ls.set(key, value),
+            removeItem: key => ls.remove(key)
+        }
+    })]
 });
+
+
+
